f6d59181a48cb5081db4f488adadb8c21fe879f4
nunmanasm <104161262+nunmanasm@users.noreply.github.com>
2022-04-22 01:04:00 +0700

    Initial commit

---
+++ src/calculator.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class calculator {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Please enter a number");
+        double number1 = input.nextDouble();
+        System.out.println("Please enter a second number");
+        double number2 = input.nextDouble();
+        double result = number1 + number2;
+        System.out.println(result);
+    }
+}
+
